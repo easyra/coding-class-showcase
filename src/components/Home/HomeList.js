711c44e5a0b497222b1f2d3f24056deff9bc6f8f
ezra davis davis <ezra.rr.davis@gmail.com>
2019-03-09 20:56:46 -0800

    rows fixed

--- src/components/Home/HomeList.js
+++ src/components/Home/HomeList.js
@@ -3,18 +3,34 @@ import HomeCard from './HomeCard';
 
 const HomeList = props => {
   const { projects } = props;
-  return (
-    <div className='row container'>
-      {projects.map(project => (
-        <HomeCard
-          title={project.title}
-          img={project.img}
-          link={project.link}
-          period={project.period}
-        />
-      ))}
-    </div>
-  );
+  function createRows() {
+    console.log(projects.length);
+    let parent = [];
+    for (let i = projects.length / 4; i > 0; i--) {
+      let children = [];
+      let startIndex = i * 4 - 1;
+      for (let j = startIndex; j > startIndex - 4; j--) {
+        console.log(j);
+        if (projects[j]) {
+          children.push(
+            <HomeCard
+              className='right'
+              title={projects[j].title}
+              img={projects[j].img}
+              link={projects[j].link}
+              period={projects[j].period}
+            />
+          );
+        }
+      }
+      parent.push(
+        <div className='row valign-wrapper container'>{children}</div>
+      );
+    }
+    console.log(parent);
+    return parent;
+  }
+  return <>{createRows()}</>;
 };
 
 export default HomeList;
